13631f4de1796f0ce5a793693db696579de95783
Ben Ramsey <benramsey25@gmail.com>
2021-07-24 19:26:45 +0930

    Add the commented hello world program.

---
+++ chapter03/examples/hello_commented.c
@@ -0,0 +1,19 @@
+/************************************************************
+ * hello_commented -- program to print out "Hello World"    *
+ *      Not an especially earth-shattering program.         *
+ *                                                          *
+ * Author: Ben Ramsey                                       *
+ * Original Author: Steve Oualline.                         *
+ *                                                          *
+ * Purpose: Demonstration of a simple program.              *
+ *                                                          *
+ * Usage:                                                   *
+ *      Runs the program and the message appears.           *
+ ************************************************************/
+#include <stdio.h>
+
+int main() {
+    // Tell the world hello
+    printf("Hello World\n");
+    return (0);
+}
